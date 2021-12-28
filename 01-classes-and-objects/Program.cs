47ceef34569c7e2369b27f396607ccd4b2c2add5
VoN <sudo.von.contact@gmail.com>
2021-12-28 12:52:00 -0600

    feat: :sparkles: Add classes and objects

---
+++ 01-classes-and-objects/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_classes_and_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+
+    class BankAccount
+    {
+        private List<Transaction> allTransactions = new List<Transaction>();
+        private static int accountNumberSeed = 1234567890;
+        public string Number {get;}
+        public string Owner {get;set;}
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+    }
+
+    class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
