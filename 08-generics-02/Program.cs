9b9577d3d1053a609a8123873bc3121bb31bbccf
VoN <sudo.von.contact@gmail.com>
2021-12-29 12:04:00 -0600

    feat: :sparkles: Add generics

---
+++ 08-generics-02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08_generics_02
+{
+    class Program
+    {
+        class Point{
+            public string x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AreEqual(2,2));
+        }
+
+        public static bool AreEqual<T>(T num1, T num2) where T:IComparable
+        {
+            return num1.CompareTo(num2) == 0;
+        }
+    }
+}
