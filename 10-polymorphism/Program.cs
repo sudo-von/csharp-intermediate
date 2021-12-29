e2b395a3b4971b73ccc4418d8b5587d83653273f
VoN <sudo.von.contact@gmail.com>
2021-12-29 15:46:38 -0600

    feat: :sparkles: Add polymorphism

---
+++ 10-polymorphism/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10_polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(new Rectangle());
+            shapes.Add(new Triangle());
+            shapes.Add(new Circle());
+            Canvas canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+        }
+    }
+
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+
+    public class Shape
+    {
+        public int Width {get;set;}
+        public int Height {get;set;}
+        public virtual void Draw()
+        {
+            Console.WriteLine("Draw a default shape");
+        }
+    }
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a triangle");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+        }
+    }
+
+}
